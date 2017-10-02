40e498fd4112eedfd7fac2c38c9fdbc450285013
bbaxa16 <brooke769@gmail.com>
2017-10-02 15:55:58 -0500

    set up angular and reconfigured login access

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,10 @@
+const app = angular.module('qtc', []);
 
+app.controller('mainController', ['$http', function($http){
+  this.admin = false;
+  this.toggleAdmin = function(){
+    this.admin = !this.admin;
+  }
+
+
+}])
